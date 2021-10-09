7d17349289a038fa2ac5590356dcb01f776fdbcc
Heliferepo <mattisdalleau@gmail.com>
2021-10-09 10:17:51 +0400

    Updating operators on vector2

--- src/vector/vector2.rs
+++ src/vector/vector2.rs
@@ -29,11 +29,7 @@ impl Vec2 {
     }
 
     pub fn scalar(&self, v: &Vec2) -> f64 {
-        *self * *v
-    }
-
-    pub fn dot(&self, v: &Vec2) -> f64 {
-        *self * *v
+        self.x * v.x + self.y * v.y
     }
 
     pub fn magnitude(&self, v: &Vec2) -> f64 {
@@ -75,12 +71,6 @@ impl SubAssign<Vec2> for Vec2 {
     }
 }
 
-// Scalar multiplication or division
-// Multiply a |v| . s (or divide)
-// where v is a vector and s a real number (a scalar)
-// Relation is |r| = |v| . s or |r| = |v| / s
-// Returns a Vector
-// Do not mismatch with vector multiplication it returns a scalar
 impl Mul<f64> for Vec2 {
     type Output = Vec2;
 
@@ -111,24 +101,22 @@ impl DivAssign<f64> for Vec2 {
     }
 }
 
-// Vector multiplication or division
-// Multiply or (divide) a |v| . |v2| where v is a vector
-// Returns a scalar
-// Relation is s = |v| . |v2| or s = |v| / |v2|
-// Do not mismatch with s * |v| multiplication it returns a vector
 impl Mul<Vec2> for Vec2 {
-    type Output = f64;
+    type Output = Vec2;
 
-    fn mul(self, rhs: Vec2) -> f64 {
-        self.x * rhs.x + self.y * rhs.y
+    fn mul(self, rhs: Vec2) -> Vec2 {
+        Vec2::new(self.x * rhs.x,
+                  self.y * rhs.y)
     }
 }
 
-/*
- * impl MulAssign<Vec2> for Vec2
- * Cannot exists because I can't assign the float to Vec
- * It could be done but it would be pretty error prone
- */
+impl MulAssign<Vec2> for Vec2 {
+
+    fn mul_assign(&mut self, rhs: Vec2) {
+        self.x *= rhs.x;
+        self.y *= rhs.y;
+    }
+}
 
 // Divisions bewteen vectors is not really necesary
 // But here it is for completeness
@@ -230,13 +218,12 @@ mod tests {
         assert_eq!(v, Vec2::new(-2.0, -2.0));
     }
 
-    // Remember that the multiplication between two vector result in a scalar
     #[test]
     fn test_mul_two_vec() {
         let v = Vec2::new(1.0, 2.0);
         let v2 = Vec2::new(3.0, 4.0);
         let s = v * v2;
-        assert_eq!(s, 1.0 * 3.0 + 2.0 * 4.0);
+        assert_eq!(s, Vec2::new(1.0 * 3.0, 2.0 * 4.0));
     }
 
     // Mul assign two vector is not possible because it returns a scalar
